91c5db347c3e01a5b388bdc359b372c72209999f
George Gastaldi <gegastaldi@gmail.com>
2017-03-18 01:30:51 -0300

    Update GitHubServicePojoIT.java

--- services/github-service-impl/src/test/java/org/kontinuity/catapult/service/github/impl/kohsuke/GitHubServicePojoIT.java
+++ services/github-service-impl/src/test/java/org/kontinuity/catapult/service/github/impl/kohsuke/GitHubServicePojoIT.java
@@ -1,6 +1,5 @@
 package org.kontinuity.catapult.service.github.impl.kohsuke;
 
-import org.kontinuity.catapult.base.identity.IdentityBuilder;
 import org.kontinuity.catapult.service.github.api.GitHubService;
 import org.kontinuity.catapult.service.github.test.GitHubTestCredentials;
 
