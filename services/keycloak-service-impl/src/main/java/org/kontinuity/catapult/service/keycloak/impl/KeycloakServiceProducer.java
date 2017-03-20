6ea8db5114e4fc3c0f0001a1d4d6255d33fbe6ac
George Gastaldi <gegastaldi@gmail.com>
2017-03-20 19:58:22 -0300

    Introduced KeycloakServiceMock

---
+++ services/keycloak-service-impl/src/main/java/org/kontinuity/catapult/service/keycloak/impl/KeycloakServiceProducer.java
@@ -0,0 +1,24 @@
+package org.kontinuity.catapult.service.keycloak.impl;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import org.kontinuity.catapult.base.EnvironmentSupport;
+import org.kontinuity.catapult.service.keycloak.api.KeycloakService;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class KeycloakServiceProducer {
+
+    @Produces
+    @ApplicationScoped
+    public KeycloakService createService() {
+        String user = EnvironmentSupport.INSTANCE.getEnvVarOrSysProp("CATAPULT_OPENSHIFT_USERNAME");
+        String password = EnvironmentSupport.INSTANCE.getEnvVarOrSysProp("CATAPULT_OPENSHIFT_PASSWORD");
+        if (user != null && password != null) {
+            return new KeycloakServiceImpl();
+        }
+        return new KeycloakServiceImpl();
+    }
+}
