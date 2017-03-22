03d6b1a99a59f956224ab79dae5373475955fb60
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-03-22 16:23:12 +0100

    still not working

--- tests/src/test/java/org/kontinuity/catapult/test/CatapultStatusResourceIT.java
+++ tests/src/test/java/org/kontinuity/catapult/test/CatapultStatusResourceIT.java
@@ -2,36 +2,30 @@ package org.kontinuity.catapult.test;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Event;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import javax.websocket.ContainerProvider;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
+import javax.ws.rs.core.UriBuilder;
 
-import com.google.common.io.Files;
-import org.glassfish.tyrus.client.ClientManager;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.Resolvers;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kontinuity.catapult.base.identity.Identity;
 import org.kontinuity.catapult.base.identity.IdentityFactory;
 import org.kontinuity.catapult.core.api.CreateProjectile;
 import org.kontinuity.catapult.core.api.ProjectileBuilder;
 import org.kontinuity.catapult.core.api.StatusMessage;
 import org.kontinuity.catapult.core.api.StatusMessageEvent;
-import org.kontinuity.catapult.service.github.test.GitHubTestCredentials;
-import org.kontinuity.catapult.web.api.CatapultStatusResource;
 
 /**
  * Validation of the {@link org.kontinuity.catapult.web.api.CatapultResource}
@@ -44,24 +38,14 @@ public class CatapultStatusResourceIT {
     private static final Logger log = Logger.getLogger(CatapultStatusResourceIT.class.getName());
 
     @Inject
-    Event<StatusMessageEvent> testEvent;
+    BeanManager manager;
 
-//    @ArquillianResource
-//    private URL deploymentUrl;
+    @ArquillianResource
+    private URI deploymentUrl;
 
     @Deployment
-    public static WebArchive getTestDeployment() {
-//        final File[] ourTestDeps = Resolvers.use(MavenResolverSystem.class)
-//                .resolve("org.java-websocket:tyrus-client:1.3.1")
-//                .withTransitivity()
-//                .asFile();
-
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                .addPackages(true, StatusMessageEvent.class.getPackage())
-                .addClass(Identity.class)
-                .addClass(StatusTestClientEndpoint.class)
-                .addClass(CatapultStatusResource.class);
+    public static WebArchive getDeployment() {
+        return Deployments.getMavenBuiltWar().addClass(StatusTestClientEndpoint.class);
     }
 
     /**
@@ -73,27 +57,25 @@ public class CatapultStatusResourceIT {
     public void websocketsStatusTest() throws Exception {
         //given
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-
-        //URI uri = new URI("ws://" + deploymentUrl.getHost() + ":" + deploymentUrl.getPort() + "/status");
-        URI uri = new URI("ws://localhost:8080/status");
+        URI uri = UriBuilder.fromUri(deploymentUrl).scheme("ws").path("status").build();
 
         StatusTestClientEndpoint endpoint = new StatusTestClientEndpoint();
         Session session = container.connectToServer(endpoint, uri);
 
         final CreateProjectile projectile = ProjectileBuilder.newInstance()
-                .gitHubIdentity(GitHubTestCredentials.getToken())
+                .gitHubIdentity(IdentityFactory.createFromToken("dummy-token"))
                 .openShiftIdentity(IdentityFactory.createFromToken("dummy-token"))
                 .openShiftProjectName("projectName")
                 .createType()
-                .projectLocation(Files.createTempDir().toPath())
+                .projectLocation(Files.createTempDirectory("dummy"))
                 .build();
 
         final HashMap<String, Object> data = new HashMap<>();
         data.put("GitHub project", "http://github.com/dummy-project-location");
 
-
         //when
-        testEvent.fire(new StatusMessageEvent(projectile, StatusMessage.GITHUB_CREATE, data));
+        manager.fireEvent(new StatusMessageEvent(projectile, StatusMessage.GITHUB_CREATE, data));
+//        testEvent.fire(new StatusMessageEvent(projectile, StatusMessage.GITHUB_CREATE, data));
         session.getBasicRemote().sendText(projectile.getId().toString());
 
         //then
