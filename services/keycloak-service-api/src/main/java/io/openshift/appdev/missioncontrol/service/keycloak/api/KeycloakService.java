1d42b1b4e9b63fd5de24bb1209264ef9ca5ede60
George Gastaldi <gegastaldi@gmail.com>
2017-08-02 20:04:31 +0000

    Fixes #264: Introduced OpenShiftClusterRegistry and OpenShiftResource
    Also, the OpenShiftServiceFactory now accepts an OpenShiftCluster when creating an OpenShiftService instance.
    
    Support OPENSHIFT_CONFIG_FILE
    
    Add KeyCloakService.getIdentity(String provider, String token)
    
    Add OpenShiftResource

--- services/keycloak-service-api/src/main/java/io/openshift/appdev/missioncontrol/service/keycloak/api/KeycloakService.java
+++ services/keycloak-service-api/src/main/java/io/openshift/appdev/missioncontrol/service/keycloak/api/KeycloakService.java
@@ -1,5 +1,7 @@
 package io.openshift.appdev.missioncontrol.service.keycloak.api;
 
+import java.util.Optional;
+
 import io.openshift.appdev.missioncontrol.base.identity.Identity;
 
 /**
@@ -11,6 +13,8 @@ public interface KeycloakService {
 
     /**
      * Returns the Openshift v3 {@link Identity} used for authenticating with the Openshift console
+     *
+     * This returns the
      *
      * @param token the keycloak access token
      * @return the openshift v3 token assigned to the given keycloak access token
@@ -24,4 +28,13 @@ public interface KeycloakService {
      * @return the github Identity token assigned to the given keycloak access token
      */
     Identity getGitHubIdentity(String token);
+
+    /**
+     * Grabs the {@link Identity} for the specified provider
+     *
+     * @param provider The identity provider to use
+     * @param token    the keycloak access token
+     * @return an {@link Optional} containing an {@link Identity}
+     */
+    Optional<Identity> getIdentity(String provider, String token);
 }
